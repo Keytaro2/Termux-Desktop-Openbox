ea979e089f2f05e04acaa53ed310e83797d7ae68
Keytaro2 <vglomy@gmail.com>
2026-05-26 04:20:23 -0600

    Update

--- config/eww/scripts/brillo.sh
+++ config/eww/scripts/brillo.sh
@@ -1,12 +1,29 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-CACHE="/data/data/com.termux/files/home/.brightness_val"
-
-# Si el archivo no existe, creamos uno al 50% (127)
-if [ ! -f "$CACHE" ]; then
-    echo "127" > "$CACHE"
+# 1. Intentamos leer los sensores de hardware directamente (rápido y siempre exacto)
+if [ -f /sys/class/leds/lcd-backlight/brightness ]; then
+    val=$(cat /sys/class/leds/lcd-backlight/brightness)
+    max=$(cat /sys/class/leds/lcd-backlight/max_brightness)
+elif [ -f /sys/class/backlight/panel0-backlight/brightness ]; then
+    val=$(cat /sys/class/backlight/panel0-backlight/brightness)
+    max=$(cat /sys/class/backlight/panel0-backlight/max_brightness)
+else
+    # 2. Si no hay acceso al hardware, usamos el lector nativo de Android
+    val=$(content query --uri content://settings/system --where "name='screen_brightness'" | grep -o 'value=[0-9]*' | cut -d'=' -f2)
+    max=255
 fi
 
-# Leemos el valor y lo convertimos a porcentaje para Eww
-val=$(cat "$CACHE")
-echo $(( val * 100 / 255 ))
+# Si por alguna razón extrema todo falla, mostramos 50% para que Eww no se rompa
+if [ -z "$val" ]; then
+    echo 50
+    exit 0
+fi
+
+# Evitar divisiones por cero en caso de error
+if [ -z "$max" ] || [ "$max" -eq 0 ]; then
+    max=255
+fi
+
+# Convertimos el valor crudo al porcentaje (0 a 100)
+echo $(( val * 100 / max ))
+
